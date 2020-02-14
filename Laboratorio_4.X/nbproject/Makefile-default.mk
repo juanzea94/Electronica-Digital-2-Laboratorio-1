87a115ccf62be3141ddf3928209dffb72a8678d2
juanzea94 <zea15401@uvg.edu.gt>
2020-02-14 15:18:22 -0600

    Inicializacion de MASTER y SLAVE

--- Laboratorio_4.X/nbproject/Makefile-default.mk
+++ Laboratorio_4.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=
+SOURCEFILES_QUOTED_IF_SPACED=Laboratorio_4.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=
-POSSIBLE_DEPFILES=
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/Laboratorio_4.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/Laboratorio_4.p1.d
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES=${OBJECTDIR}/Laboratorio_4.p1
 
 # Source Files
-SOURCEFILES=
+SOURCEFILES=Laboratorio_4.c
 
 
 
@@ -94,7 +94,23 @@ MP_PROCESSOR_OPTION=16F887
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/Laboratorio_4.p1: Laboratorio_4.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/Laboratorio_4.p1.d 
+	@${RM} ${OBJECTDIR}/Laboratorio_4.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1    -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/Laboratorio_4.p1 Laboratorio_4.c 
+	@-${MV} ${OBJECTDIR}/Laboratorio_4.d ${OBJECTDIR}/Laboratorio_4.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/Laboratorio_4.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
+${OBJECTDIR}/Laboratorio_4.p1: Laboratorio_4.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/Laboratorio_4.p1.d 
+	@${RM} ${OBJECTDIR}/Laboratorio_4.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c    -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/Laboratorio_4.p1 Laboratorio_4.c 
+	@-${MV} ${OBJECTDIR}/Laboratorio_4.d ${OBJECTDIR}/Laboratorio_4.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/Laboratorio_4.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 endif
 
 # ------------------------------------------------------------------------------------
